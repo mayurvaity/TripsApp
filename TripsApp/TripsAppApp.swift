1410a8598c91f948453410ebba5cb10cdd689946
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-08-09 22:15:50 +0530

    Initial Commit

---
+++ TripsApp/TripsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripsAppApp.swift
+//  TripsApp
+//
+//  Created by Mayur Vaity on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TripsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
