bc6d12df640b438259e11813d197a50f8cf28729
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-08-09 23:15:17 +0530

    Implement list vw for trips list.

--- TripsApp/ContentView.swift
+++ TripsApp/ContentView.swift
@@ -6,17 +6,60 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    
+    @Query var trips: [Trip]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(trips) { trip in
+                    VStack(alignment: .leading) {
+                        Text(trip.name)
+                            .font(.title2)
+                            
+                        
+                        HStack {
+                            Text(trip.startDate.formatted(date: .abbreviated, time: .omitted))
+                                .font(.callout)
+                            
+                            Spacer()
+                            
+                            Image(systemName: "arrow.right")
+                                .font(.title3)
+                            
+                            Spacer()
+                            
+                            Text(trip.endDate.formatted(date: .abbreviated, time: .omitted))
+                                .font(.callout)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Trips")
+            .toolbar {
+                Button("Add samples") {
+                    addSamples()
+                }
+            }
         }
-        .padding()
+        
     }
+    
+    func addSamples() {
+        let rome = Trip(name: "Rome")
+        let nyc = Trip(name: "NYC")
+        let paris = Trip(name: "Paris")
+        
+        modelContext.insert(rome)
+        modelContext.insert(nyc)
+        modelContext.insert(paris)
+    }
+    
+    
 }
 
 #Preview {
